2591145a1d4314a22658b916a583a20f0428fb45
Jason Merten <jmerte@gmail.com>
2025-02-09 07:18:39 -0500

    added json flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,21 @@
 use anyhow::Result;
 
-pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+pub fn find_matches(
+    content: &str,
+    pattern: &str,
+    mut writer: impl std::io::Write,
+    json: bool,
+) -> Result<()> {
     for line in content.lines() {
         if line.contains(&pattern) {
+            if json {
+                writeln!(
+                    writer,
+                    "{}",
+                    serde_json::json!({"type":"match", "content": line})
+                )?;
+                continue;
+            }
             writeln!(writer, "{}", line)?;
         }
     }
@@ -11,13 +24,33 @@ pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Writ
 
 #[cfg(test)]
 mod tests {
+    use serde_json::json;
+
     use crate::find_matches;
 
     #[test]
     fn find_a_match() {
         let mut result = Vec::new();
-        find_matches("i am a banana\ni am an orange", "banana", &mut result)
-            .expect("error encountered");
+        find_matches(
+            "i am a banana\ni am an orange",
+            "banana",
+            &mut result,
+            false,
+        )
+        .expect("error encountered");
         assert_eq!(result, b"i am a banana\n");
+    }
+
+    #[test]
+    fn find_a_match_json() {
+        let mut result = Vec::new();
+        find_matches("i am a banana\ni am an orange", "banana", &mut result, true)
+            .expect("error encountered");
+
+        let expected = json!({"content":"i am a banana","type":"match"}).to_string();
+        assert_eq!(
+            String::from_utf8(result).unwrap(),
+            format!("{}\n", expected)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,18 @@ use signal_hook::iterator::Signals;
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
+
+    #[arg(long = "json")]
+    json: bool,
 }
 
 fn main() -> Result<()> {
     let mut signals = Signals::new([signal_hook::consts::SIGINT, signal_hook::consts::SIGTERM])?;
 
     thread::spawn(move || {
-        for sig in signals.forever() {
-            println!("Received signal {:?}: ", sig)
-        }
+        signals
+            .forever()
+            .for_each(|sig| println!("Received signal {:?}: ", sig));
     });
 
     let args = Cli::parse();
@@ -24,6 +27,6 @@ fn main() -> Result<()> {
     let file_content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    rust_cli::find_matches(&file_content, &args.pattern, std::io::stdout())?;
+    rust_cli::find_matches(&file_content, &args.pattern, std::io::stdout(), args.json)?;
     Ok(())
 }
